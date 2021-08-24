4b2d8519afa0a4ba614cd45d13b2a38b8bb2b1e6
unknown <rankumar@altametrics.com>
2021-08-24 01:14:24 -0700

    project setup

---
+++ layout/footer.php
@@ -0,0 +1,51 @@
+
+    <div class="copy-right-area">
+        <div class="container">
+            <div class="copy-right-content">
+                <p>
+                    Copyright © 2021 Finix. Designed By
+                    <a href="https://envytheme.com/" target="_blank">
+                        EnvyTheme
+                    </a>
+                </p>
+            </div>
+        </div>
+    </div>
+
+
+    <div class="go-top">
+        <i class='bx bx-chevron-up'></i>
+    </div>
+
+
+    <script data-cfasync="false" src="../../cdn-cgi/scripts/5c5dd728/cloudflare-static/email-decode.min.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+
+    <script src="assets/js/popper.min.js"></script>
+
+    <script src="assets/js/bootstrap.min.js"></script>
+
+    <script src="assets/js/jquery.meanmenu.js"></script>
+
+    <script src="assets/js/owl.carousel.min.js"></script>
+
+    <script src="assets/js/jquery.nice-select.min.js"></script>
+
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>
+
+    <script src="assets/js/odometer.min.js"></script>
+
+    <script src="assets/js/jquery.appear.min.js"></script>
+
+    <script src="assets/js/jquery.ajaxchimp.min.js"></script>
+
+    <script src="assets/js/form-validator.min.js"></script>
+
+    <script src="assets/js/contact-form-script.js"></script>
+
+    <script src="assets/js/wow.min.js"></script>
+
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
---
+++ layout/header.php
@@ -0,0 +1,125 @@
+<!doctype html>
+<html lang="en">
+
+
+<head>
+
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
+    <link rel="stylesheet" href="assets/css/animate.min.css">
+
+    <link rel="stylesheet" href="assets/css/meanmenu.css">
+
+    <link rel="stylesheet" href="assets/css/boxicons.min.css">
+
+    <link rel="stylesheet" href="assets/css/flaticon.css">
+
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+
+    <link rel="stylesheet" href="assets/css/owl.theme.default.min.css">
+
+    <link rel="stylesheet" href="assets/css/magnific-popup.min.css">
+
+    <link rel="stylesheet" href="assets/css/nice-select.min.css">
+
+    <link rel="stylesheet" href="assets/css/odometer.min.css">
+
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <link rel="stylesheet" href="assets/css/responsive.css">
+    <title>Finix - Loans and Funding Agency HTML Template</title>
+    <link rel="icon" type="image/png" href="assets/img/favicon.png">
+</head>
+
+<body>
+
+    <div class="navbar-area">
+        <div class="main-responsive-nav">
+            <div class="container">
+                <div class="main-responsive-menu">
+                    <div class="logo">
+                        <a href="index-2.html">
+                            <img src="assets/img/logo.png" alt="image">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="main-navbar">
+            <div class="container-fluid">
+                <nav class="navbar navbar-expand-md navbar-light">
+                    <a class="navbar-brand" href="index-2.html">
+                        <img src="assets/img/logo.png" alt="image">
+                    </a>
+                    <div class="collapse navbar-collapse mean-menu" id="navbarSupportedContent">
+                        <ul class="navbar-nav">
+                            <li class="nav-item">
+                                <a href="index.php" class="nav-link active">
+                                    Home                                    
+                                </a>                                
+                            </li>
+                            <li class="nav-item">
+                                <a href="about.php" class="nav-link active">
+                                    About                                    
+                                </a>                                
+                            </li>                            
+                            <li class="nav-item">
+                                <a href="#" class="nav-link">
+                                    Services
+                                    <i class='bx bx-chevron-down'></i>
+                                </a>
+                                <ul class="dropdown-menu">
+                                    <li class="nav-item">
+                                        <a href="services.html" class="nav-link">
+                                            Services
+                                        </a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a href="services-details.html" class="nav-link">
+                                            Services Details
+                                        </a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li class="nav-item">
+                                <a href="funding.html" class="nav-link">
+                                    Funding
+                                </a>
+                            </li>
+                            <li class="nav-item">
+                                <a href="loans.html" class="nav-link">
+                                    Loans
+                                </a>
+                            </li>                        
+                            <li class="nav-item">
+                                <a href="contact.html" class="nav-link">
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+                        <div class="others-options d-flex align-items-center">
+                            
+                            <div class="option-item">
+                                <div class="info">
+                                    <i class="flaticon-telephone"></i>
+                                    <span>Call Now</span>
+                                    <p>
+                                        <a href="tel:1514312-5678">+1 (514) 312-5678</a>
+                                    </p>
+                                </div>
+                            </div>
+                            <div class="option-item">
+                                <a href="apply-now.html" class="default-btn">
+                                    Apply now
+                                    <span></span>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+            </div>
+        </div>
+    </div>
