81707073f123e5c0a01f9b0fdd873695562978fa
susheelsahoo <susheelcs24@gmail.com>
2021-09-28 14:18:16 +0530

    map

--- layout/footer.php
+++ layout/footer.php
@@ -121,8 +121,8 @@
 
 
     
-    <script src="assets/js/jquery.min.js"></script>
-
+    <!-- <script src="assets/js/jquery.min.js"></script> -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="assets/js/popper.min.js"></script>
 
     <script src="assets/js/bootstrap.min.js"></script>
